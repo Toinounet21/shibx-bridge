d4637c5ff1066969357e42c078b0de3ac635ac62
Toinounet21 <79363334+Toinounet21@users.noreply.github.com>
2023-11-13 11:38:56 +0100

    Add files via upload

---
+++ src/config/mainnet.ts
@@ -0,0 +1,16 @@
+import {ChainId} from '@layerzerolabs/lz-sdk';
+import {Token} from 'x-shibx-bridge';
+
+const tokens = [
+  new Token(ChainId.ARBITRUM, '0x371c7ec6D8039ff7933a2AA28EB827Ffe1F52f07', 18, 'SHIBX'),
+  new Token(ChainId.AVALANCHE, '0x6e84a6216ea6dacc71ee8e6b0a5b7322eebc0fdd', 18, 'SHIBX'),
+];
+
+const proxy = [
+  {
+    chainId: ChainId.AVALANCHE,
+    address: '0x371c7ec6D8039ff7933a2AA28EB827Ffe1F52f07',
+  },
+];
+
+export const mainnet = {tokens, proxy};
---
+++ src/config/testnet.ts
@@ -0,0 +1,16 @@
+import {ChainId} from '@layerzerolabs/lz-sdk';
+import {Token} from 'x-shibx-bridge';
+
+const tokens = [
+  new Token(ChainId.FUJI, '0x6B23b44032eeabE2d4B543Fd043458a1f4a97C67', 18, 'SHIBX'),
+  new Token(ChainId.ARBITRUM_GOERLI, '0x13A60f9bb70adcF2D72b8132e6F011AbeFA68490', 18, 'SHIBX'),
+];
+
+const proxy = [
+  {
+    chainId: ChainId.FUJI,
+    address: '0x1589a034c8698FE7381364d1b0eE0e1E8a0bA369',
+  },
+];
+
+export const testnet = {tokens, proxy};
