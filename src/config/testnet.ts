ccfae26a2423e19af85d23e5a5cb9812f2853cc8
Toinounet21 <79363334+Toinounet21@users.noreply.github.com>
2023-11-13 23:33:56 +0100

    Update testnet.ts

--- src/config/testnet.ts
+++ src/config/testnet.ts
@@ -2,14 +2,14 @@ import {ChainId} from '@layerzerolabs/lz-sdk';
 import {Token} from 'x-shibx-bridge';
 
 const tokens = [
-  new Token(ChainId.FUJI, '0x6B23b44032eeabE2d4B543Fd043458a1f4a97C67', 18, 'SHIBX'),
-  new Token(ChainId.ARBITRUM_GOERLI, '0x13A60f9bb70adcF2D72b8132e6F011AbeFA68490', 18, 'SHIBX'),
+  new Token(ChainId.FUJI, '0x567A0c028cED53645637E4414c99c6F433860C35', 18, 'SHIBX'),
+  new Token(ChainId.ARBITRUM_GOERLI, '0xe33dADD0727372b6A8Afe46430C821C01a575c28', 18, 'SHIBX'),
 ];
 
 const proxy = [
   {
     chainId: ChainId.FUJI,
-    address: '0x1589a034c8698FE7381364d1b0eE0e1E8a0bA369',
+    address: '0x5164db46A18C8680393b6df36c0BCc724078a4D9',
   },
 ];
 
